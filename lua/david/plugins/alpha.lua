b6efb1359710541f77e2c0970c05cec54b22af67
dmoriley <david.m.oriley@hotmail.com>
2024-01-06 14:14:52 -0500

    Added project plugin for quick changing projects

--- lua/david/plugins/alpha.lua
+++ lua/david/plugins/alpha.lua
@@ -26,15 +26,16 @@ return {
         }
 
         dashboard.section.buttons.val = {
-            dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-            dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("f", "  Find file", "<CMD>Telescope find_files <CR>"),
+            dashboard.button("e", "  New file", "<CMD>ene <BAR> startinsert <CR>"),
+            dashboard.button("r", "  Recently used files", "<CMD>Telescope oldfiles <CR>"),
+            dashboard.button("t", "  Find text", "<CMD>Telescope live_grep <CR>"),
+            dashboard.button("p", "📁  Find Project", "<CMD>Telescope projects<CR>"),
             dashboard.button("c", "  Configuration", "<CMD>cd $HOME/.config/nvim/ | lua require('telescope.builtin').find_files()<CR>"),
-            dashboard.button("o", "  Current Session", ":lua require('persistence').load()<CR>"),
-            dashboard.button("l", "  Last Session", ":lua require('persistence').load({ last = true })<CR>"),
+            dashboard.button("o", "  Current Directory Session", "<CMD>lua require('persistence').load()<CR>"),
+            dashboard.button("l", "⏪  Last Session", "<CMD>lua require('persistence').load({ last = true })<CR>"),
             dashboard.button("x", "  File Explorer", "<cmd>Oil<CR>"),
-            dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+            dashboard.button("q", "  Quit Neovim", "<CMD>qa<CR>"),
         }
 
         vim.api.nvim_create_autocmd("User", {

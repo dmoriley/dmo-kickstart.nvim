9a91137d0c4ce422f7ef49bc93dfc42511c4d037
dmoriley <david.m.oriley@hotmail.com>
2024-01-18 23:42:02 -0500

    ui.lua file forgot to add

---
+++ lua/david/ui.lua
@@ -0,0 +1,4 @@
+vim.cmd([[colorscheme onedark]])
+-- vim.cmd([[colorscheme tokyonight]])
+vim.api.nvim_set_hl(0, "CursorLine", { underline = true })
+-- vim.cmd([[highlight! CursorLine gui=underline]])

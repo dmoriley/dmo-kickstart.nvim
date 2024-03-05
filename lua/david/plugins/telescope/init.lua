e5ddf38fc2ebb7e7fe24c061767846ac11953142
dmoriley <david.m.oriley@hotmail.com>
2024-03-05 16:02:32 -0500

    Added telescope file browser

--- lua/david/plugins/telescope/init.lua
+++ lua/david/plugins/telescope/init.lua
@@ -1,132 +1,131 @@
 return {
-  -- Fuzzy Finder (files, lsp, etc)
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  -- cmd = 'Telescope'
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    {
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      'nvim-telescope/telescope-fzf-native.nvim',
-      -- NOTE: If you are having trouble with this installation,
-      --       refer to the README for telescope-fzf-native for more instructions.
-      build = 'make',
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      cond = function()
-        return vim.fn.executable 'make' == 1
-      end,
+  {
+    -- Fuzzy Finder (files, lsp, etc)
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    -- cmd = 'Telescope'
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        -- Only load if `make` is available. Make sure you have the system
+        -- requirements installed.
+        cond = function()
+          return vim.fn.executable('make') == 1
+        end,
+      },
     },
-  },
-  config = function()
-    local actions = require 'telescope.actions'
-    local custom_pickers = require 'david.plugins.telescope.custom_pickers'
-    require('telescope').setup {
-      defaults = {
-        layout_strategy = 'flex',
-        layout_config = {
-          horizontal = {
-            preview_width = 0.55,
-          },
-          flex = {
-            -- breakpoints for flex layout, when to swap between vertical and horizontal
-            -- flip_lines = 20,
-            flip_columns = 170,
-          },
-        },
-        mappings = {
-          i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
-            -- after the selection of a telescope item, center the cursor vertically on the buffer window
-            ['<CR>'] = actions.select_default + actions.center,
-          },
-          n = {
-            ['<CR>'] = actions.select_default + actions.center,
-          },
-        },
-      },
-      pickers = {
-        help_tags = {
-          mappings = {
-            n = {
-              ['<CR>'] = actions.select_vertical,
-            },
-            i = {
-              ['<CR>'] = actions.select_vertical,
-            },
-          },
-        },
-        oldfiles = {
-          sort_lastused = true,
-        },
-        find_files = {
-          path_display = { 'truncate' },
-          find_command = {
-            'rg',
-            '--files',
-            '--color',
-            'never',
-          },
-        },
-        git_files = {
-          show_untracked = true,
-        },
-        live_grep = {
-          -- ripgrep is the default tool for live_grep and grep_string
-          path_display = { truncate = 3 },
-          mappings = {
-            i = {
-              ['<C-k>'] = custom_pickers.actions.set_extension,
-              ['<C-i>'] = custom_pickers.actions.set_ignore_extension,
-            },
-          },
-          -- consider disabling treesitter in preview window for performance
-          -- preview = {
-          --     treesitter = false
-          -- }
-        },
-        grep_string = {
-          path_display = { truncate = 3 },
-        },
-        buffers = {
-          sort_lastused = true,
-          sort_mru = true,
-          ignore_current_buffer = true,
-          theme = 'dropdown',
-          previewer = false,
-          mappings = {
-            i = { ['<C-d>'] = actions.delete_buffer },
-            n = { ['<C-d>'] = actions.delete_buffer },
-          },
-        },
-        builtin = {
-          theme = 'dropdown',
-          include_extensions = true,
-          use_default_opts = true,
+    config = function()
+      local actions = require('telescope.actions')
+      local custom_pickers = require('david.plugins.telescope.custom_pickers')
+      require('telescope').setup({
+        defaults = {
+          layout_strategy = 'flex',
           layout_config = {
-            width = 50,
-            height = 0.5,
+            horizontal = {
+              preview_width = 0.55,
+            },
+            flex = {
+              -- breakpoints for flex layout, when to swap between vertical and horizontal
+              -- flip_lines = 20,
+              flip_columns = 170,
+            },
+          },
+          mappings = {
+            i = {
+              ['<C-u>'] = false,
+              ['<C-d>'] = false,
+              -- after the selection of a telescope item, center the cursor vertically on the buffer window
+              ['<CR>'] = actions.select_default + actions.center,
+            },
+            n = {
+              ['<CR>'] = actions.select_default + actions.center,
+            },
           },
         },
-        diagnostics = {
-          sort_by = 'severity',
+        pickers = {
+          help_tags = {
+            mappings = {
+              n = {
+                ['<CR>'] = actions.select_vertical,
+              },
+              i = {
+                ['<CR>'] = actions.select_vertical,
+              },
+            },
+          },
+          oldfiles = {
+            sort_lastused = true,
+          },
+          find_files = {
+            path_display = { 'truncate' },
+            find_command = {
+              'rg',
+              '--files',
+              '--color',
+              'never',
+            },
+          },
+          git_files = {
+            show_untracked = true,
+          },
+          live_grep = {
+            -- ripgrep is the default tool for live_grep and grep_string
+            path_display = { truncate = 3 },
+            mappings = {
+              i = {
+                ['<C-k>'] = custom_pickers.actions.set_extension,
+                ['<C-i>'] = custom_pickers.actions.set_ignore_extension,
+              },
+            },
+            -- consider disabling treesitter in preview window for performance
+            -- preview = {
+            --     treesitter = false
+            -- }
+          },
+          grep_string = {
+            path_display = { truncate = 3 },
+          },
+          buffers = {
+            sort_lastused = true,
+            sort_mru = true,
+            ignore_current_buffer = true,
+            theme = 'dropdown',
+            previewer = false,
+            mappings = {
+              i = { ['<C-d>'] = actions.delete_buffer },
+              n = { ['<C-d>'] = actions.delete_buffer },
+            },
+          },
+          builtin = {
+            theme = 'dropdown',
+            include_extensions = true,
+            use_default_opts = true,
+            layout_config = {
+              width = 50,
+              height = 0.5,
+            },
+          },
+          diagnostics = {
+            sort_by = 'severity',
+          },
         },
-      },
-    }
+      })
 
-    -- Enable telescope fzf native, if installed
-    require('telescope').load_extension 'fzf'
+      -- Enable telescope fzf native, if installed
+      require('telescope').load_extension('fzf')
 
-    local keymap = vim.keymap.set
-    local options = function(description)
-      if description then
-        return { noremap = true, silent = true, desc = description }
+      local keymap = vim.keymap.set
+      local options = function(description)
+        if description then
+          return { noremap = true, silent = true, desc = description }
+        end
+        return { noremap = true, silent = true }
       end
-      return { noremap = true, silent = true }
-    end
 
-    --[[
+      --[[
         -- Telescope live_grep in git root
         -- Function to find the git root directory based on the current buffer's path
         local function find_git_root()
@@ -169,53 +168,56 @@ return {
 
         ]]
 
-    local function telescope_live_grep_open_files()
-      require('telescope.builtin').live_grep {
-        grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-      }
-    end
-    keymap('n', '<leader>s/', telescope_live_grep_open_files, options 'Search by Grep in open files')
-    keymap('n', '<leader>sg', custom_pickers.custom_live_grep, options '[S]earch by Grep')
-    keymap('n', '<leader>so', require('telescope.builtin').oldfiles, options '[S]earch recently [o]pened files')
-    keymap('n', '<leader><space>', require('telescope.builtin').buffers, options '[ ] Find existing buffers')
-    keymap('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find, options 'Current Buffer Fuzzy')
-    keymap('n', '<leader>sz', require('telescope.builtin').builtin, options '[S]earch Telescope builtin')
-    keymap('n', '<leader>sf', require('telescope.builtin').find_files, options '[S]earch Files')
-    keymap('n', '<leader>sh', require('telescope.builtin').help_tags, options '[S]earch Help')
-    keymap('n', '<leader>sr', require('telescope.builtin').resume, options '[S]earch Resume')
-    -- Search the nvim config workspace to tweak things on the fly
-    keymap('n', '<leader>sx', function()
-      require('telescope.builtin').find_files { cwd = '~/.config/nvim' }
-    end, options 'Search nvim config workspace')
+      local function telescope_live_grep_open_files()
+        require('telescope.builtin').live_grep({
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        })
+      end
+      keymap('n', '<leader>s/', telescope_live_grep_open_files, options('Search by Grep in open files'))
+      keymap('n', '<leader>sg', custom_pickers.custom_live_grep, options('[S]earch by Grep'))
+      keymap('n', '<leader>so', require('telescope.builtin').oldfiles, options('[S]earch recently [o]pened files'))
+      keymap('n', '<leader><space>', require('telescope.builtin').buffers, options('[ ] Find existing buffers'))
+      keymap('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find, options('Current Buffer Fuzzy'))
+      keymap('n', '<leader>sz', require('telescope.builtin').builtin, options('[S]earch Telescope builtin'))
+      keymap('n', '<leader>sf', require('telescope.builtin').find_files, options('[S]earch Files'))
+      keymap('n', '<leader>sh', require('telescope.builtin').help_tags, options('[S]earch Help'))
+      keymap('n', '<leader>sr', require('telescope.builtin').resume, options('[S]earch Resume'))
+      -- Search the nvim config workspace to tweak things on the fly
+      keymap('n', '<leader>sx', function()
+        require('telescope.builtin').find_files({ cwd = '~/.config/nvim' })
+      end, options('Search nvim config workspace'))
 
-    keymap('n', '<leader>sdd', function()
-      require('telescope.builtin').diagnostics { bufnr = 0 }
-    end, options '[S]earch [D]ocument [D]iagnostics')
-    keymap('n', '<leader>sds', require('telescope.builtin').lsp_document_symbols, options '[S]earch [D]ocument [S]ymbols')
-    keymap('n', '<leader>sws', require('telescope.builtin').lsp_dynamic_workspace_symbols, options '[S]earch [W]orkspace [S]ymbols')
-    keymap('n', '<leader>sk', require('telescope.builtin').keymaps, options 'Search Keymaps')
-    keymap('n', '<leader>sww', require('telescope.builtin').grep_string, options 'Search workspace for current word')
-    keymap('n', '<leader>sc', require('telescope.builtin').commands, options 'Search commands')
-    -- vim.keymap.set('x', '<leader>sww',  '"zy<Cmd>Telescope live_grep<CR><C-r>z', options('Search workspace for selection') ) -- alternative search workspace for selection
-    keymap('n', '<leader>sm', require('telescope.builtin').marks, options '[S]earch marks')
-    keymap(
-      'x',
-      '<leader>sww',
-      '"zy<Cmd>lua require("telescope.builtin").grep_string({search=vim.fn.getreg("z")})<CR>',
-      options 'Search workspace for selection'
-    )
-    keymap('n', 'gr', function()
-      require('telescope.builtin').lsp_references { fname_width = 50, trim_text = false }
-    end, options 'Goto References')
-    -- lsp_definitions is async so the zz command happens before definitions completes. Need to look into how to do this. For now it doesn't center
-    keymap('n', 'gd', "<CMD>lua require('telescope.builtin').lsp_definitions()<CR> <bar> <CMD> lua vim.cmd.normal('zz')<CR>", options 'Goto Definition')
-    keymap('n', 'gtd', require('telescope.builtin').lsp_type_definitions, options 'Goto Type Definition')
-    keymap('n', 'gI', require('telescope.builtin').lsp_implementations, options 'Goto Implementation')
-    keymap('n', '<leader>sds', require('telescope.builtin').lsp_document_symbols, options 'Search Document Symbols')
-    keymap('n', '<leader>sws', require('telescope.builtin').lsp_dynamic_workspace_symbols, options 'Search Workspace Symbols')
+      keymap('n', '<leader>sdd', function()
+        require('telescope.builtin').diagnostics({ bufnr = 0 })
+      end, options('[S]earch [D]ocument [D]iagnostics'))
+      keymap('n', '<leader>sds', require('telescope.builtin').lsp_document_symbols, options('[S]earch [D]ocument [S]ymbols'))
+      keymap('n', '<leader>sws', require('telescope.builtin').lsp_dynamic_workspace_symbols, options('[S]earch [W]orkspace [S]ymbols'))
+      keymap('n', '<leader>sk', require('telescope.builtin').keymaps, options('Search Keymaps'))
+      keymap('n', '<leader>sww', require('telescope.builtin').grep_string, options('Search workspace for current word'))
+      keymap('n', '<leader>sc', require('telescope.builtin').commands, options('Search commands'))
+      -- vim.keymap.set('x', '<leader>sww',  '"zy<Cmd>Telescope live_grep<CR><C-r>z', options('Search workspace for selection') ) -- alternative search workspace for selection
+      keymap('n', '<leader>sm', require('telescope.builtin').marks, options('[S]earch marks'))
+      keymap(
+        'x',
+        '<leader>sww',
+        '"zy<Cmd>lua require("telescope.builtin").grep_string({search=vim.fn.getreg("z")})<CR>',
+        options('Search workspace for selection')
+      )
+      keymap('n', 'gr', function()
+        require('telescope.builtin').lsp_references({ fname_width = 50, trim_text = false })
+      end, options('Goto References'))
+      -- lsp_definitions is async so the zz command happens before definitions completes. Need to look into how to do this. For now it doesn't center
+      keymap('n', 'gd', "<CMD>lua require('telescope.builtin').lsp_definitions()<CR> <bar> <CMD> lua vim.cmd.normal('zz')<CR>", options('Goto Definition'))
+      keymap('n', 'gtd', require('telescope.builtin').lsp_type_definitions, options('Goto Type Definition'))
+      keymap('n', 'gI', require('telescope.builtin').lsp_implementations, options('Goto Implementation'))
+      keymap('n', '<leader>sds', require('telescope.builtin').lsp_document_symbols, options('Search Document Symbols'))
+      keymap('n', '<leader>sws', require('telescope.builtin').lsp_dynamic_workspace_symbols, options('Search Workspace Symbols'))
 
-    -- git stuff
-    keymap('n', '<leader>gf', require('telescope.builtin').git_files, options 'Search Git Files')
-  end,
+      -- git stuff
+      keymap('n', '<leader>gf', require('telescope.builtin').git_files, options('Search Git Files'))
+    end,
+  },
+  -- telescope dep, but seperate from dep table so it can be lazy loaded
+  require('david.plugins.telescope.telescope_file_browser'),
 }
--- lua/david/plugins/undotree.lua
+++ lua/david/plugins/undotree.lua
@@ -1,11 +1,11 @@
 return {
-    "mbbill/undotree",
-    keys = {
-        { "<leader>u", ":UndotreeToggle<cr>", mode = "n", desc = "Undotree toggle", noremap = true, silent = true }
-    },
-    cmd = {'UndotreeToggle', 'UndotreeShow'},
-    config = function()
-        vim.g.undotree_SetFocusWhenToggle = 1
-        vim.g.undotree_WindowLayout = 2
-    end
+  'mbbill/undotree',
+  keys = {
+    { '<leader>u', '<CMD>UndotreeToggle<cr>', mode = 'n', desc = 'Undotree toggle', noremap = true, silent = true },
+  },
+  cmd = { 'UndotreeToggle', 'UndotreeShow' },
+  config = function()
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_WindowLayout = 2
+  end,
 }

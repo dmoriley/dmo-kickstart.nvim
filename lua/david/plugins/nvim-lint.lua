26953558bcd7b6e6bc90166ed8b40de621d3038b
dmoriley <david.m.oriley@hotmail.com>
2024-03-12 22:59:30 -0400

    feat: added nvim-lint plugin
    For linting javascript and typescript files

---
+++ lua/david/plugins/nvim-lint.lua
@@ -0,0 +1,34 @@
+return {
+  'mfussenegger/nvim-lint',
+  -- event = {
+  --   'BufReadPre',
+  --   'BufNewFile',
+  -- },
+  ft = { 'javascriptreact', 'typescriptreact', 'javascript', 'typescript' },
+  config = function()
+    local lint = require('lint')
+
+    lint.linters_by_ft = {
+      javascript = { 'eslint' },
+      typescript = { 'eslint' },
+      javascriptreact = { 'eslint' },
+      typescriptreact = { 'eslint' },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
+    -- trigger linting on buffer enter, right after writing to a buffer, and when leaving insert mode
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      -- could define a pattern to limit the linting to
+      -- pattern = {'*.js', '*.ts'},
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    vim.keymap.set({ 'n', 'v' }, '<leader>dl', function()
+      lint.try_lint()
+    end, { noremap = true, silent = true, desc = 'Document lint' })
+  end,
+}

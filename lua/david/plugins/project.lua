78039a8b78a525bf379430af7177035bf242ef38
dmoriley <david.m.oriley@hotmail.com>
2024-04-21 23:55:51 -0400

    Formatting and lock update

--- lua/david/plugins/project.lua
+++ lua/david/plugins/project.lua
@@ -1,19 +1,19 @@
 return {
-    {
-        "ahmedkhalf/project.nvim",
-        event = 'VeryLazy',
-        opts = {
-            manual_mode = false,
-        },
-        dependencies = {
-            'nvim-telescope/telescope.nvim'
-        },
-        config = function()
-            require("project_nvim").setup()
-            require("telescope").load_extension("projects")
-        end,
-        keys = {
-            { "<leader>sp", "<Cmd>Telescope projects<CR>", mode = 'n', desc = 'Search Projects', noremap = true, silent = true},
-        },
-    }
+  {
+    'ahmedkhalf/project.nvim',
+    event = 'VeryLazy',
+    opts = {
+      manual_mode = false,
+    },
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require('project_nvim').setup()
+      require('telescope').load_extension('projects')
+    end,
+    keys = {
+      { '<leader>sp', '<Cmd>Telescope projects<CR>', mode = 'n', desc = 'Search Projects', noremap = true, silent = true },
+    },
+  },
 }

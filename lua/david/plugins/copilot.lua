bbc1998ca2030939eda3f11de59cb334b2ad333d
dmoriley <david.m.oriley@hotmail.com>
2024-07-11 19:10:58 -0400

    Changed copilot plugin and removed telescope file browser

--- lua/david/plugins/copilot.lua
+++ lua/david/plugins/copilot.lua
@@ -1,5 +1,21 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
   cmd = 'Copilot',
   event = 'InsertEnter',
+  config = function()
+    require('copilot').setup({
+      debounce = 100,
+      suggestion = {
+        keymap = {
+          accept = '<C-Enter>',
+          next = '<C-j>',
+          prev = '<C-k>',
+          dismiss = '<C-c>',
+        },
+      },
+      filetypes = {
+        markdown = true,
+      },
+    })
+  end,
 }

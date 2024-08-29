672b2436cbda66daca876873c69b47dda5ab3ec3
dmoriley <david.m.oriley@hotmail.com>
2024-08-28 21:25:16 -0400

    Updated comform config

--- lua/david/plugins/conform.lua
+++ lua/david/plugins/conform.lua
@@ -2,9 +2,9 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local conform = require 'conform'
+    local conform = require('conform')
 
-    conform.setup {
+    conform.setup({
       format_on_save = {
         timeout_ms = 500,
         lsp_fallback = false,
@@ -12,26 +12,26 @@ return {
       },
       formatters_by_ft = {
         -- sub-list to run only the first available formatter
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-        javascriptreact = { { 'prettierd', 'prettier' } },
-        typescriptreact = { { 'prettierd', 'prettier' } },
-        css = { { 'prettierd', 'prettier' } },
-        html = { { 'prettierd', 'prettier' } },
-        json = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
         lua = { 'stylua' },
         go = { 'goimports', 'golines' }, -- goimports also does gofmt
       },
-    }
+    })
 
     -- normal mode: format whole file
     -- visual mode: format selection
     vim.keymap.set({ 'n', 'v' }, '<leader>df', function()
-      conform.format {
+      conform.format({
         timeout_ms = 500,
         lsp_fallback = false,
         async = false,
-      }
+      })
     end, { noremap = true, silent = true, desc = 'Document format or range (in visual mode)' })
   end,
 }

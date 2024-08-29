59044c0f25560c489836c39c150369de3f75ba40
dmoriley <david.m.oriley@hotmail.com>
2024-08-28 22:45:50 -0400

    Added duplicate and comment out keymap

--- lua/david/plugins/init.lua
+++ lua/david/plugins/init.lua
@@ -18,6 +18,26 @@ return {
     'numToStr/Comment.nvim',
     event = 'BufReadPre',
     opts = {},
+    config = function()
+      -- @see https://github.com/numToStr/Comment.nvim/issues/70
+      vim.keymap.set('n', 'gC', function()
+        local count = vim.v.count1
+        local api = require('Comment.api')
+        vim.api.nvim_feedkeys('V', 'n', false)
+        if count > 1 then
+          vim.api.nvim_feedkeys((count - 1) .. 'j', 'n', false)
+        end
+        vim.api.nvim_feedkeys('yP', 'nx', false)
+        api.comment.linewise.count(count)
+        vim.api.nvim_feedkeys('`<^i', 'n', false)
+      end, { noremap = true, silent = true, desc = 'Duplicate and comment selected text' })
+      vim.keymap.set(
+        'x',
+        'gC',
+        'V<Esc>gvy`>pgv<Esc>`><cmd>lua require"Comment.api".comment.linewise("V")<CR><Down>^i',
+        { noremap = true, silent = true, desc = 'Duplicate and comment selected text' }
+      )
+    end,
   },
   {
     'folke/persistence.nvim',

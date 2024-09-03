dc66d596542c6fa7f7efa50a32dec17748245c51
dmoriley <david.m.oriley@hotmail.com>
2024-09-03 10:22:10 -0400

    Extracted comment nvim to its own file

---
+++ lua/david/plugins/comment.lua
@@ -0,0 +1,27 @@
+return {
+  -- commenting supported natively in neovim 0.10, but doesnt do block comments yet
+  -- can probably remove when that is available
+  'numToStr/Comment.nvim',
+  event = 'BufReadPre',
+  config = function()
+    require('Comment').setup()
+    -- @see https://github.com/numToStr/Comment.nvim/issues/70
+    vim.keymap.set('n', 'gC', function()
+      local count = vim.v.count1
+      local api = require('Comment.api')
+      vim.api.nvim_feedkeys('V', 'n', false)
+      if count > 1 then
+        vim.api.nvim_feedkeys((count - 1) .. 'j', 'n', false)
+      end
+      vim.api.nvim_feedkeys('yP', 'nx', false)
+      api.comment.linewise.count(count)
+      vim.api.nvim_feedkeys('`<^i', 'n', false)
+    end, { noremap = true, silent = true, desc = 'Duplicate and comment selected text' })
+    vim.keymap.set(
+      'x',
+      'gC',
+      'V<Esc>gvy`>pgv<Esc>`><cmd>lua require"Comment.api".comment.linewise("V")<CR><Down>^i',
+      { noremap = true, silent = true, desc = 'Duplicate and comment selected text' }
+    )
+  end,
+}

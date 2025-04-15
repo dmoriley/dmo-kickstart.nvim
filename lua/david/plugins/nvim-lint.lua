d8d3d2a38e0298287a8ebb4e19d42f00b0de4272
dmoriley <david.m.oriley@hotmail.com>
2025-04-15 10:44:45 -0400

    Commented out lint configs

--- lua/david/plugins/nvim-lint.lua
+++ lua/david/plugins/nvim-lint.lua
@@ -41,17 +41,17 @@ return {
       end,
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    -- local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
     -- trigger linting on buffer enter, right after writing to a buffer, and when leaving insert mode
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-      -- could define a pattern to limit the linting to
-      pattern = { '*.js', '*.ts', '*.tsx', '*.jsx' },
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+    -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+    --   -- could define a pattern to limit the linting to
+    --   pattern = { '*.js', '*.ts', '*.tsx', '*.jsx' },
+    --   group = lint_augroup,
+    --   callback = function()
+    --     lint.try_lint()
+    --   end,
+    -- })
 
     -- print out running linters
     vim.api.nvim_create_user_command('PrintLintersInfo', function()

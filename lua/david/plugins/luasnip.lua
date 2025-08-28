6c93d235e635339c22e08662a678c2d8edc93eed
dmoriley <david.m.oriley@hotmail.com>
2025-08-27 21:57:04 -0400

    Moved luasnip lazy load to config and added filetype extends

--- lua/david/plugins/luasnip.lua
+++ lua/david/plugins/luasnip.lua
@@ -8,17 +8,23 @@ return {
     'rafamadriz/friendly-snippets',
     config = function()
       require('luasnip.loaders.from_vscode').lazy_load()
-      require('luasnip.loaders.from_vscode').lazy_load({
-        paths = {
-          vim.fn.stdpath('config') .. '/snippets', -- global custom snippets located ~/.config/snippets
-          vim.fn.getcwd() .. '/.vscode', -- project snippets in the  current_working_dir/.vscode
-          vim.fn.getcwd() .. '/.snippets', -- project snippets in the  current_working_dir/.vscode
-        },
-      })
     end,
   },
   opts = {
     history = true,
     delete_check_events = 'TextChanged',
   },
+  config = function()
+    require('luasnip.loaders.from_vscode').lazy_load({
+      paths = {
+        vim.fn.stdpath('config') .. '/snippets', -- global custom snippets located ~/.config/snippets
+        vim.fn.getcwd() .. '/.vscode', -- project snippets in the  current_working_dir/.vscode
+        vim.fn.getcwd() .. '/.snippets', -- project snippets in the  current_working_dir/.vscode
+      },
+    })
+    local luasnip = require('luasnip')
+    luasnip.filetype_extend('lua', { 'luadoc' })
+    luasnip.filetype_extend('typescript', { 'tsdoc', 'javascript' })
+    luasnip.filetype_extend('javascript', { 'jsdoc' })
+  end,
 }

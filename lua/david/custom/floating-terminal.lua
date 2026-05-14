ae10465eab4e40c2cc1d00e4ed9f0f4b1ab39b36
dmoriley <david.m.oriley@hotmail.com>
2026-05-14 09:52:12 -0400

    custom(floating-terminal) open in inset and disable alternate file

--- lua/david/custom/floating-terminal.lua
+++ lua/david/custom/floating-terminal.lua
@@ -9,15 +9,26 @@ local state = {
 }
 
 local toggle_terminal = function()
+  -- check if a valid window has already been created (or already opened)
   if not vim.api.nvim_win_is_valid(state.floating.win) then
     state.floating = open_floating_window({ buf = state.floating.buf })
     if vim.bo[state.floating.buf].buftype ~= 'terminal' then
       -- call terminal inside of the new buffer
       vim.cmd.terminal()
     end
-    vim.bo[state.floating.buf].buflisted = false
-    vim.cmd('startinsert')
+    vim.bo[state.floating.buf].buflisted = false -- don't list buffer so it doens't appear in tab list
+
+    -- disable some keymaps in normal mode for the floating buffer window
+    for _, lhs in ipairs({ '<Tab>', '<C-6>', '<C-^>' }) do
+      vim.keymap.set('n', lhs, '<Nop>', {
+        buffer = state.floating.buf,
+        desc = 'Disable alternate buffer in floating terminal',
+      })
+    end
+
+    vim.cmd.startinsert() -- start in insert mode
   else
+    -- window already opened so hide it
     vim.api.nvim_win_hide(state.floating.win)
   end
 end

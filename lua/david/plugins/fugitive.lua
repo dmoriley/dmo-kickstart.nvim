6a4bebea471b77c70a1d9009176cc2e501d40424
dmoriley <david.m.oriley@hotmail.com>
2026-07-23 15:46:11 -0400

    chore(fugitive) - new keymaps

--- lua/david/plugins/fugitive.lua
+++ lua/david/plugins/fugitive.lua
@@ -4,11 +4,37 @@ return {
   keys = {
     {
       '<leader>gg',
-      '<cmd>tabnew<cr><cmd>vertical Git<cr><cmd>vertical resize 60<cr>',
+      function()
+        if vim.bo.filetype == 'fugitive' then
+          vim.cmd('tabclose')
+          return
+        end
+
+        vim.cmd('tab Git')
+      end,
       mode = 'n',
-      desc = 'Open Git Fugitive in new tab',
+      desc = 'Toggle Git Fugitive tab',
       noremap = true,
       silent = true,
     },
   },
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'fugitive',
+      callback = function(event)
+        vim.keymap.set('n', 'q', '<cmd>close<cr>', {
+          buffer = event.buf,
+          desc = 'Close Fugitive',
+          silent = true,
+        })
+
+        for _, mode in ipairs({ 'n', 'x', 'o' }) do
+          -- Fugitive exposes its original actions through <Plug> mappings. remap = true expands those
+          -- mappings, allowing gu and gU to invoke each other's actions instead of literal key input.
+          vim.keymap.set(mode, 'gu', '<Plug>fugitive:gU', { buffer = event.buf, remap = true }) -- jump to Unstaged files
+          vim.keymap.set(mode, 'gU', '<Plug>fugitive:gu', { buffer = event.buf, remap = true }) -- jump to Untracked files
+        end
+      end,
+    })
+  end,
 }
